40cd7dd5ef6d9d6362d24c4acd39565b44896a41
ingebrixen <info@ingebrixen.de>
2021-11-15 20:02:16 +0100

    new

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**  (c) Thomas Böhme **/
+
+define('DBUSER','root');
+define('DBPWD','root');
+define('DBNAME','db');
+define('DBHOST','db');
+
+define('HOST', 'https://homebuddy.ddev.site');
+
+define('regOption', false); //Anlegen neuer Accounts erlaubt?
