7459702d861dffea57cbd108604e195105fb1cf1
ingebrixen <info@ingebrixen.de>
2021-12-04 08:50:10 +0100

    smallChanges

--- htdocs/lib/Util/Tops.php
+++ htdocs/lib/Util/Tops.php
@@ -15,7 +15,9 @@ class Tops
         $_model = 'Finanzen';
         $_table = 'sumAusg';
         $_colum = 'wer, sumAusgaben';
+
         $query = 'SELECT '.$_colum.' FROM '.$_table;
+
         $resourceModel = \App::getResourceModel('DBHandler');
         $dataSet = $resourceModel->selectTops($_model, $query);
         
@@ -26,7 +28,9 @@ class Tops
         $_model = 'Finanzen';
         $_table = 'persKonto';
         $_colum = 'konto, lend';
+
         $query = 'SELECT '.$_colum.' FROM '.$_table.' WHERE id = '.$userId;
+
         $resourceModel = \App::getResourceModel('DBHandler');
         $dataSet = $resourceModel->selectTops($_model, $query);
         
@@ -37,7 +41,9 @@ class Tops
         $_model = 'Finanzen';
         $_table = 'haushaltskasse';
         $_colum = 'stand';
-        $query = 'SELECT '.$_colum.' FROM '.$_table.' ORDER BY ID DESC LIMIT 1' ;
+
+        $query = 'SELECT '.$_colum.' FROM '.$_table.' ORDER BY ID DESC LIMIT 1';
+
         $resourceModel = \App::getResourceModel('DBHandler');
         $dataSet = $resourceModel->selectTops($_model, $query);
         
@@ -51,7 +57,7 @@ class Tops
                 $konto = \abs($konto); 
                 $text = "Du musst <b>{$konto}&#8364;</b> einzahlen!";
                 return  $array = ['text' => $text,
-                                 'max' => $konto,
+                                 'max' => '',
                                  'val' => $konto,
                                  'show' => ''];
                 break;
